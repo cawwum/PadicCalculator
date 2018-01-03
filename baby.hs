be527943e98f774e810c3f8b735ad29296b5c30b
Callum Gray <callumgray96@gmail.com>
2018-01-03 17:51:34 +0000

    Add files via upload
    multip

--- baby.hs
+++ baby.hs
@@ -1,5 +1,11 @@
+-- CONSTANTS
+
 p = 10
 pminus1 = (p-1)
+pminus1s = pminus1:pminus1s
+ones = 1:ones
+
+-- ADDITION
 
 add' :: [Integer] -> [Integer] -> Integer -> [Integer]
 add' [] [] 0 = []
@@ -13,6 +19,20 @@ add' (x:xs) (y:ys) n = if((x+y+n) < p) then (x+y+n):(add' xs ys 0) else ((x+y+n)
 add :: [Integer] -> [Integer] -> [Integer]
 add xs ys = add' xs ys 0
 
+-- SUBTRACTION
+
+sub :: [Integer] -> [Integer] -> [Integer]
+sub [] [] = []
+sub [] ys = sub (0:[]) ys
+sub xs [] = xs 
+sub (x:xs) (y:ys) = if(x >= y) then (x-y):(sub xs ys) else (p+x-y):(sub (borrow xs) ys)
+
+borrow :: [Integer] -> [Integer]
+borrow [] = pminus1s
+borrow (x:xs) = if(x>0)then(x-1):xs else (p-1):(borrow xs)
+
+-- MULTIPLICATION
+
 addLoop :: Integer -> [Integer] -> [Integer]
 addLoop 0 _  = []
 addLoop n xs = add xs (addLoop (n-1) xs)
@@ -25,15 +45,7 @@ mult :: [Integer] -> [Integer] -> [Integer]
 mult [] _ = []
 mult (x:xs) ys = addShift (addLoop x ys) (mult xs ys)
 
-sub :: [Integer] -> [Integer] -> [Integer]
-sub [] [] = []
-sub [] ys = sub (0:[]) ys
-sub xs [] = xs 
-sub (x:xs) (y:ys) = if(x >= y) then (x-y):(sub xs ys) else (p+x-y):(sub (borrow xs) ys)
+-- DIVISION
 
-borrow :: [Integer] -> [Integer]
-borrow [] = pminus1s
-borrow (x:xs) = if(x>0)then(x-1):xs else (p-1):(borrow xs)
 
-pminus1s = pminus1:pminus1s
-ones = 1:ones
+
